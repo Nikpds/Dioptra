58be291358889e9d849e7ca28f15f3e2aa9ca987
afyselias <ag.fyselias@gmail.com>
2019-06-11 14:25:51 +0300

    Server files - prototype

--- dioptra.adminpanel/src/components/containers/DashboardContainer.js
+++ dioptra.adminpanel/src/components/containers/DashboardContainer.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ServerCard from '../ServerCard.js';
 
 const DashboardContainer = () => {
+
+  //Server Data
+  const [myData, setMyData] = useState(
+    [{
+      name: "Server 1",
+      status: 'online',
+      ip: "11.1.48.249"
+    },
+    {
+      name: "Server2",
+      status: "online",
+      ip: "11.1.51.7"
+    },
+    {
+      name: "Server 3",
+      status: "offline",
+      ip: "11.1.48.217"
+    },
+    {
+      name: "Server 4",
+      status: "offline",
+      ip: "11.1.48.157"
+    }]);
+
+    const servs = myData.map((item) => <ServerCard name={item.name} status={item.status} ip={item.ip} />)
+
     return (
         <div>
-          This is Dashboard Container  
+          This is Dashboard Container 
+          {servs}
         </div>
     );
 };
